7503ed22140eda2de6a2e40aa2846068c60202db
Davi Queiroz Albuquerque <daviqueiroz2002.dq@gmail.com>
2023-06-30 10:31:29 -0300

    API: opção de deletar usuário e tratando erros dos serviços ao cliente

--- src/api/controllers/user_controller.js
+++ src/api/controllers/user_controller.js
@@ -1,10 +1,34 @@
 import userService from "../services/user_service.js";
+import { NotFoundError, ConflictError, InternalServerError } from "../errors/errors.js";
 
 export async function createUser(req, res, next) {
-    const user = req.body;
-    const createdUser = await userService.createUser(user);
+    try {
+        const user = req.body;
+        const createdUser = await userService.createUser(user);
 
-    res.status(201).json(createdUser);
+        res.status(201).json(createdUser);
+    }
+    catch (err) {
+        if(err instanceof ConflictError || err instanceof InternalServerError)
+            return res.status(err.status).json(err.body).end();
+        
+        return res.status(500).json({status: 500, message: "Something gone wrong"}).end();
+    }
+}
+
+export async function deleteUser(req, res, next) {
+    const userId = parseInt(req.params.userId);
+    
+    try{
+        const deletedUser = await userService.deleteUser(userId);
+        return res.status(200).json(deletedUser);
+    }
+    catch(err) {
+        if(err instanceof NotFoundError || err instanceof InternalServerError)
+            return res.status(err.status).json(err.body).end();
+        
+        return res.status(500).json({status: 500, message: "Something gone wrong"}).end();
+    }
 }
 
 export async function getAllUsers(req, res, next) {
@@ -14,15 +38,23 @@ export async function getAllUsers(req, res, next) {
         password: req.query.password
     };
     const users = await userService.getAllUsers(filters);
-
-    return res.json(users);
+    
+    return res.status(200).json(users);
 }
 
 export async function getUser(req, res, next) {
     const userId = parseInt(req.params.userId);
-    const user = await userService.getUser(userId);
-
-    return user ? res.status(200).json(user) : res.status(404).end();
+    
+    try {
+        const user = await userService.getUser(userId);
+        return res.status(200).json(user);
+    } 
+    catch (err) {
+        if(err instanceof NotFoundError || err instanceof InternalServerError)
+            return res.status(err.status).json(err.body).end();
+    
+        return res.status(500).json({status: 500, message: "Something gone wrong"}).end();
+    }
 }
 
 export async function getUserPlaylists(req, res, next) {
--- src/api/services/user_service.js
+++ src/api/services/user_service.js
@@ -1,26 +1,73 @@
 import prisma from "../configs/database.js"
+import { NotFoundError, ConflictError, InternalServerError } from "../errors/errors.js";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library.js";
 
 async function createUser(user) {
-    const createdUser = await prisma.user.create({
-        data: {
-            ...user,
-            playlists: {
-                create: {
-                    title: 'Músicas Curtidas'
+    try {
+        const createdUser = await prisma.user.create({
+            data: {
+                ...user,
+                playlists: {
+                    create: {
+                        title: 'Músicas Curtidas'
+                    }
                 }
             }
-        }
-    });
+        });
+    
+        return createdUser;    
+    } 
+    catch (err) {
+        if(err instanceof PrismaClientKnownRequestError && err.code === 'P2002')
+            throw new ConflictError(`User with ${err.meta.target[0]} ${user[err.meta.target[0]]} already exists`);
+        
+        throw new InternalServerError();
+    }
+}
 
-    return createdUser;
+async function deleteUser(userId) {
+    try {
+        const deletedUser = await prisma.user.delete({
+            where: {
+                id: userId
+            },
+            select: {
+                id: true,
+                email: true,
+                name: true,
+                playlists: {
+                    select: {
+                        id: true,
+                        title: true
+                    }
+                }
+            }
+        });
+    
+        return deletedUser;    
+    } 
+    catch (err) {
+        if(err instanceof PrismaClientKnownRequestError && err.code === 'P2025')
+            throw new NotFoundError(`User with id ${userId} not found`);
+        
+        throw new InternalServerError();
+    }
 }
 
 async function getUser(userId) {
-    const user = await prisma.user.findUnique({
-        where: {id: userId}
-    });
-
-    return user;
+    try {
+        const user = await prisma.user.findUniqueOrThrow({
+            where: {id: userId}
+        });
+    
+        return user;    
+    } 
+    catch (err) {
+        if(err instanceof PrismaClientKnownRequestError && err.code === 'P2025')
+            throw new NotFoundError(`User with id ${userId} not found`);
+        
+            throw new InternalServerError;
+    }
 }
 
 async function getAllUsers(filters) {
@@ -58,6 +105,7 @@ async function getUserPlaylists(userId){
 
 const userService = {
     createUser,
+    deleteUser,
     getUser,
     getAllUsers,
     getUserPlaylists
